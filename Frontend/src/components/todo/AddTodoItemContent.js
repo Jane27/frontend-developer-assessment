8909307e06ffd45ef64cc5921ecb530ebf64f19a
jane27 <wangjing27@outlook.com>
2023-11-11 12:44:15 +1300

    Update add todo item content with form validation and clear.

--- Frontend/src/components/todo/AddTodoItemContent.js
+++ Frontend/src/components/todo/AddTodoItemContent.js
@@ -3,48 +3,65 @@ import { Button, Container, Row, Col, Form, Stack } from 'react-bootstrap';
 
 const AddTodoItemContent = () => {
   const [description, setDescription] = useState('');
+  const [validated, setValidated] = useState(false);
+
   const handleDescriptionChange = (event) => {
-    // todo
+    setDescription(event.target.value);
   };
 
-  async function handleAdd() {
-    try {
-      alert('todo');
-    } catch (error) {
-      console.error(error);
+  async function handleAdd(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    // Update form to display validation result
+    setValidated(true);
+
+    const form = event.currentTarget;
+    if (!form.checkValidity()) {
+      return;
     }
+
+    // Clear form after succeed
+    handleClear();
   }
 
   function handleClear() {
     setDescription('');
+
+    // Reset form validation status
+    setValidated(false);
   }
 
   return (
     <Container>
       <h1>Add Item</h1>
-      <Form.Group as={Row} className="mb-3" controlId="formAddTodoItem">
-        <Form.Label column sm="2">
-          Description
-        </Form.Label>
-        <Col md="6">
-          <Form.Control
-            type="text"
-            placeholder="Enter description..."
-            value={description}
-            onChange={handleDescriptionChange}
-          />
-        </Col>
-      </Form.Group>
-      <Form.Group as={Row} className="mb-3 offset-md-2" controlId="formAddTodoItem">
-        <Stack direction="horizontal" gap={2}>
-          <Button variant="primary" onClick={() => handleAdd()}>
-            Add Item
-          </Button>
-          <Button variant="secondary" onClick={() => handleClear()}>
-            Clear
-          </Button>
-        </Stack>
-      </Form.Group>
+      <Form noValidate validated={validated} onSubmit={handleAdd}>
+        <Form.Group as={Row} className="mb-3" controlId="formAddTodoItem">
+          <Form.Label column sm="2">
+            Description
+          </Form.Label>
+          <Col md="6">
+            <Form.Control
+              required
+              type="text"
+              placeholder="Enter description..."
+              value={description}
+              onChange={handleDescriptionChange}
+            />
+            <Form.Control.Feedback type="invalid">Description is required.</Form.Control.Feedback>
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row} className="mb-3 offset-md-2" controlId="formAddTodoItem">
+          <Stack direction="horizontal" gap={2}>
+            <Button variant="primary" type="submit">
+              Add Item
+            </Button>
+            <Button variant="secondary" onClick={() => handleClear()}>
+              Clear
+            </Button>
+          </Stack>
+        </Form.Group>
+      </Form>
     </Container>
   );
 };
